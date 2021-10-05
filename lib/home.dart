013e05d1beb637ef7015e43927b19343e18df7ac
AnneRoki <59899443+AnneRoki@users.noreply.github.com>
2021-10-05 11:28:20 +0800

    Initial commit

---
+++ lib/home.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return Scaffold(
+        body: GridView.count(
+      mainAxisSpacing: 15,
+      crossAxisSpacing: 10,
+      crossAxisCount: 1,
+      children: [
+        Card(
+          child: Column(
+            children: [
+              ListTile(
+                title: Text('Account Savings'),
+                subtitle: Text('Amount'),
+              ),
+            ],
+          ),
+        )
+      ],
+    ));
+  }
+}
---
+++ lib/splash.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutterexam/home.dart';
+
+class splash extends StatefulWidget {
+  const splash({Key? key}) : super(key: key);
+  @override
+  _splashState createState() => _splashState();
+}
+
+@override
+class _splashState extends State<splash> {
+  void initState() {
+    super.initState();
+    navigatetohome();
+  }
+
+//this is to move to the home screen
+  navigatetohome() async {
+    // the duration of the splash screen
+    await Future.delayed(Duration(milliseconds: 5000), () {});
+    // the code to go to the next screen/page
+    Navigator.pushReplacement(
+        context, MaterialPageRoute(builder: (context) => Home()));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 100,
+              width: 100,
+              color: Colors.blue[400],
+            ),
+            Container(
+              child: Text(
+                'Splash Screen',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
