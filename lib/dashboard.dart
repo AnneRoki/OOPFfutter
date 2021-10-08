61546f44ea1dd8cc8e4552cb614bdd9b2338783a
AnneRoki <59899443+AnneRoki@users.noreply.github.com>
2021-10-08 11:27:09 +0800

    Update dashboard.dart

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutterexam/ammar.dart';
 
-class Dashboard extends StatelessWidget {
+class Dashboard extends StatefulWidget {
+  const Dashboard({Key? key}) : super(key: key);
+
+  @override
+  _DashboardState createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
