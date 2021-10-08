ded0847917b9014aedac70d335ac964c583e53d8
AnneRoki <59899443+AnneRoki@users.noreply.github.com>
2021-10-08 13:37:28 +0800

    Update profpage.dart

--- lib/profpage.dart
+++ lib/profpage.dart
@@ -30,78 +30,72 @@ class ProfilePageState extends State<ProfilePage> {
       backgroundColor: Colors.yellow[100],
       body: Padding(
         padding: EdgeInsets.all(25),
-        child: Column(
-          children: [
-            Container(
-              height: 50,
-              width: 700,
-              decoration: BoxDecoration(
-                  color: Colors.yellow[100],
-                  borderRadius: BorderRadius.all(Radius.circular(10))
-                  ),
-              child: Padding(
-                padding: EdgeInsets.all(10),
-                child: Text(
-                  'Details',
-                  style: TextStyle(fontSize: 20, color: Colors.black), 
-                ),
-              ),  
-            ), 
-            Padding(padding: EdgeInsets.all(5)),
-            Container(
-              height: 120,
-              width: 600,
-              decoration: BoxDecoration(
-                  color: Colors.yellow,
-                  borderRadius: BorderRadius.all(Radius.circular(10))
-                  ),
-              child: Padding(
-                padding: EdgeInsets.all(10),
-                child: Text(
-                  'Name:            Juan Dela Cruz'+ 
-                  '\nBirth date:      5/25/1995'+
-                  '\nNationality:    Filipino' +
-                  '\nAddress:        Davao City',
-                  style: TextStyle(fontSize: 21, color: Colors.black), 
-                ),
+        child: Column(children: [
+          Container(
+            height: 50,
+            width: 700,
+            decoration: BoxDecoration(
+                color: Colors.yellow[100],
+                borderRadius: BorderRadius.all(Radius.circular(10))),
+            child: Padding(
+              padding: EdgeInsets.all(10),
+              child: Text(
+                'Details',
+                style: TextStyle(fontSize: 20, color: Colors.black),
               ),
             ),
-            Padding(padding: EdgeInsets.all(5)),
-            Container(
-              height: 50,
-              width: 700,
-              decoration: BoxDecoration(
-                  color: Colors.yellow[100],
-                  borderRadius: BorderRadius.all(Radius.circular(10))
-                  ),
-              child: Padding(
-                padding: EdgeInsets.all(10),
-                child: Text(
-                  'Your card',
-                  style: TextStyle(fontSize: 20, color: Colors.black), 
-                ),
-              ),  
-            ), 
-            Padding(padding: EdgeInsets.all(5)),
-            Container(
-              height: 215, 
-              width: 700,
-              decoration: BoxDecoration(
-                  color: Colors.black,
-                  borderRadius: BorderRadius.all(Radius.circular(10))
-                  ),
-                  child: Padding(
-                padding: EdgeInsets.all(10),
-                child: Text(
-                  'AIT Bank' + 
-                  '\n\n\nXXXXXX XXXX XXXX 12'+
-                  '\n\n\nJuan Dela Cruz',
-                  style: TextStyle(fontSize: 20, color: Colors.blueGrey), 
-                ),
-              ),  
-            ), 
-          ]
-      ),
+          ),
+          Padding(padding: EdgeInsets.all(5)),
+          Container(
+            height: 120,
+            width: 600,
+            decoration: BoxDecoration(
+                color: Colors.yellow,
+                borderRadius: BorderRadius.all(Radius.circular(10))),
+            child: Padding(
+              padding: EdgeInsets.all(10),
+              child: Text(
+                'Name:            Juan Dela Cruz' +
+                    '\nBirth date:      5/25/1995' +
+                    '\nNationality:    Filipino' +
+                    '\nAddress:        Davao City',
+                style: TextStyle(fontSize: 21, color: Colors.black),
+              ),
+            ),
+          ),
+          Padding(padding: EdgeInsets.all(5)),
+          Container(
+            height: 50,
+            width: 700,
+            decoration: BoxDecoration(
+                color: Colors.yellow[100],
+                borderRadius: BorderRadius.all(Radius.circular(10))),
+            child: Padding(
+              padding: EdgeInsets.all(10),
+              child: Text(
+                'Your card',
+                style: TextStyle(fontSize: 20, color: Colors.black),
+              ),
+            ),
+          ),
+          Padding(padding: EdgeInsets.all(5)),
+          Container(
+            height: 215,
+            width: 700,
+            decoration: BoxDecoration(
+                color: Colors.black,
+                borderRadius: BorderRadius.all(Radius.circular(10))),
+            child: Padding(
+              padding: EdgeInsets.all(10),
+              child: Text(
+                'AIT Bank' +
+                    '\n\n\nXXXXXX XXXX XXXX 12' +
+                    '\n\n\nJuan Dela Cruz',
+                style: TextStyle(fontSize: 20, color: Colors.blueGrey),
+              ),
+            ),
+          ),
+        ]),
       ),
     );
   }
